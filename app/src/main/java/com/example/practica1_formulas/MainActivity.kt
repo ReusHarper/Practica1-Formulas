601bdc81981e85ae8cdcff97ecbfb90f7f5859c5
ReusHarper <reusharper@comunidad.unam.mx>
2022-10-16 21:04:19 -0500

    Reestablecimiento de estilos de SplashScreen dentro de archivo themes

--- app/src/main/java/com/example/practica1_formulas/MainActivity.kt
+++ app/src/main/java/com/example/practica1_formulas/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Set the default theme when the app is load and the splash theme when the app is start
+        setTheme(R.style.Theme_Practica1Formulas)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
