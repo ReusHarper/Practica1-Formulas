95b0a52401dba955bcca520219ecf9c1f9982638
ReusHarper <reusharper@comunidad.unam.mx>
2022-10-21 15:58:44 -0500

    Creacion de Vista Principal: Establecimiento de spinner con imagen de formula y campos de textos

--- app/src/main/java/com/example/practica1_formulas/MainActivity.kt
+++ app/src/main/java/com/example/practica1_formulas/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.practica1_formulas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var spinner: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         // Set the default theme when the app is load & the splash theme when the app starts
